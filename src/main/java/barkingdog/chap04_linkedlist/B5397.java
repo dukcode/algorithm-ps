e8b9f442323f8d72e6bf80f67676503805623495
dukcode <duk9741@gmail.com>
2022-07-14 21:17:14 +0900

    B5397

---
+++ src/main/java/barkingdog/chap04_linkedlist/B5397.java
@@ -0,0 +1,62 @@
+package barkingdog.chap04_linkedlist;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class B5397 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            String str = br.readLine();
+            LinkedList<Character> ll = new LinkedList<>();
+            ListIterator<Character> it = ll.listIterator();
+            for (int i = 0; i < str.length(); ++i) {
+                char ch = str.charAt(i);
+
+                switch (ch) {
+                    case '<':
+                        if (it.hasPrevious()) {
+                            it.previous();
+                        }
+                        break;
+                    case '>':
+                        if (it.hasNext()) {
+                            it.next();
+                        }
+                        break;
+                    case '-':
+                        if (it.hasPrevious()) {
+                            it.previous();
+                            it.remove();
+                        }
+                        break;
+                    default:
+                        it.add(ch);
+                        break;
+                }
+            }
+
+            for (Character c : ll) {
+                bw.write(c);
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+}
