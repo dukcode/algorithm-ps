e15596434f3c001315ee4e11e3445bc222832315
dukcode <duk9741@gmail.com>
2022-07-16 15:40:16 +0900

    B3986

---
+++ src/main/java/barkingdog/chap07_stack_pair/B3986.java
@@ -0,0 +1,45 @@
+package barkingdog.chap07_stack_pair;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class B3986 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+        while (n-- > 0) {
+            String str = br.readLine();
+
+            Stack<Character> stk = new Stack<>();
+            for (int i = 0; i < str.length(); ++i) {
+                char c = str.charAt(i);
+
+                if (stk.isEmpty() || stk.peek() != c) {
+                    stk.push(c);
+                    continue;
+                }
+                stk.pop();
+            }
+
+            ans += stk.isEmpty() ? 1 : 0;
+        }
+
+        bw.write(String.valueOf(ans));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+}
