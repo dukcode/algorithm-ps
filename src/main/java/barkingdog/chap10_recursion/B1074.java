d831e33609cc30061461cd63081752eb8d071eff
dukcode <duk9741@gmail.com>
2022-07-28 02:05:16 +0900

    B1074

---
+++ src/main/java/barkingdog/chap10_recursion/B1074.java
@@ -0,0 +1,50 @@
+package barkingdog.chap10_recursion;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B1074 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(getCount(n, r, c)));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int getCount(int n, int r, int c) {
+        if (n == 0) {
+            return 0;
+        }
+
+        int half = 1 << (n - 1);
+
+        if (r < half && c < half) {
+            return getCount(n - 1, r, c);
+        }
+        if (r < half && c >= half) {
+            return half * half + getCount(n - 1, r, c - half);
+        }
+
+        if (r >= half && c < half) {
+            return 2 * half * half + getCount(n - 1, r - half, c);
+        }
+
+        return 3 * half * half + getCount(n - 1, r - half, c - half);
+    }
+
+}
