fcf27673a8582a43e5c7716bfddfcb1c2854f9b1
dukcode <duk9741@gmail.com>
2022-07-28 17:25:02 +0900

    B2448

---
+++ src/main/java/barkingdog/chap10_recursion/B2448.java
@@ -0,0 +1,56 @@
+package barkingdog.chap10_recursion;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B2448 {
+
+    static boolean[][] image;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        image = new boolean[n][2 * n - 1];
+
+        func(0, n - 1, n);
+
+        for (int y = 0; y < n; ++y) {
+            for (int x = 0; x < 2 * n - 1; ++x) {
+                if (image[y][x]) {
+                    bw.write('*');
+                } else {
+                    bw.write(' ');
+                }
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void func(int r, int c, int n) {
+
+        if (n == 3) {
+            image[r][c] = true;
+            image[r + 1][c - 1] = true;
+            image[r + 1][c + 1] = true;
+            for (int x = c - 2; x <= c + 2; ++x) {
+                image[r + 2][x] = true;
+            }
+            return;
+        }
+
+        func(r, c, n / 2);
+        func(r + n / 2, c - n / 2, n / 2);
+        func(r + n / 2, c + n / 2, n / 2);
+    }
+
+}
