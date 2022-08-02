fba8af9945fe2e16d9812cc3d06ae9410e2834a7
dukcode <duk9741@gmail.com>
2022-08-02 11:14:02 +0900

    B17298 review

---
+++ src/main/java/review/chap05_stack/B17298.java
@@ -0,0 +1,51 @@
+package review.chap05_stack;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class B17298 {
+
+    static int[] arr;
+    static int[] ans;
+
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        ans = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stk = new Stack<>();
+        for (int i = n - 1; i >= 0; --i) {
+            int idx = i + 1;
+
+            while (idx < n && arr[i] >= arr[idx]) {
+                idx = ans[idx];
+            }
+            ans[i] = idx;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            int idx = ans[i];
+            bw.write(String.valueOf(idx == n ? -1 : arr[idx]));
+            bw.write(' ');
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
