374c4c41f273321592fa9a04d317634b081665c5
dukcode <duk9741@gmail.com>
2022-08-16 07:47:01 +0900

    B6198 review

--- src/main/java/review/chap05_stack/B6198.java
+++ src/main/java/review/chap05_stack/B6198.java
@@ -7,13 +7,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Stack;
 
-
-public class B1874 {
+public class B6198 {
 
     private static BufferedReader br;
     private static BufferedWriter bw;
 
     private static int n;
+
+    private static long ans;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,33 +23,21 @@ public class B1874 {
 
         n = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
         Stack<Integer> stk = new Stack<>();
-        int i = 1;
         while (n-- > 0) {
             int num = Integer.parseInt(br.readLine());
 
-            while (stk.isEmpty() || stk.peek() < num) {
-                stk.push(i);
-                i++;
-                sb.append("+\n");
-            }
-
-            if (stk.peek() == num) {
+            while (!stk.isEmpty() && stk.peek() <= num) {
                 stk.pop();
-                sb.append("-\n");
-            } else {
-                sb = new StringBuilder("NO");
-                break;
             }
 
+            ans += stk.size();
+            stk.push(num);
         }
 
-        bw.write(sb.toString());
+        bw.write(String.valueOf(ans));
 
-        bw.flush();
         bw.close();
         br.close();
     }
-
 }
