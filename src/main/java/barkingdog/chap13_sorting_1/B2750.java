a267949c6be6f90af6f1313d88197c14fba8859a
dukcode <duk9741@gmail.com>
2022-08-31 18:24:26 +0900

    B2750

---
+++ src/main/java/barkingdog/chap13_sorting_1/B2750.java
@@ -0,0 +1,48 @@
+package barkingdog.chap13_sorting_1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B2750 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    private static int n;
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // br = new BufferedReader(new FileReader("input.txt"));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = n - 1; i > 0; --i) {
+            for (int j = 0; j < i; ++j) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            bw.write(String.valueOf(arr[i]));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
