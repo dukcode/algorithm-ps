a48174f6773d8e751e96b15add06fb3876efbc73
dukcode <duk9741@gmail.com>
2022-09-04 17:19:21 +0900

    B11656

---
+++ src/main/java/barkingdog/chap14_sorting_2/B11656.java
@@ -0,0 +1,40 @@
+package barkingdog.chap14_sorting_2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class B11656 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // br = new BufferedReader(new FileReader("input.txt"));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine();
+        String[] suffixes = new String[word.length()];
+
+        for (int i = 0; i < word.length(); ++i) {
+            String suffix = word.substring(i);
+            suffixes[i] = suffix;
+        }
+
+        Arrays.sort(suffixes);
+
+        for (int i = 0; i < word.length(); ++i) {
+            bw.write(suffixes[i]);
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
