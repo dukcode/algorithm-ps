4b5a0b5a6aaa35adcc79118b9b52d272c42e4dbf
dukcode <duk9741@gmail.com>
2022-09-08 17:00:43 +0900

    B11053

---
+++ src/main/java/barkingdog/chap15_dp/B11053.java
@@ -0,0 +1,52 @@
+package barkingdog.chap15_dp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B11053 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringTokenizer st;
+
+    private static int n;
+    private static int[] arr;
+    private static int[] cache;
+
+    private static int ans;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // br = new BufferedReader(new FileReader("input.txt"));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        cache = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+
+            cache[i] = 1;
+            for (int j = 0; j < i; ++j) {
+                if (arr[i] > arr[j]) {
+                    cache[i] = Math.max(cache[i], cache[j] + 1);
+                }
+            }
+
+            ans = Math.max(ans, cache[i]);
+        }
+
+        bw.write(String.valueOf(ans));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
