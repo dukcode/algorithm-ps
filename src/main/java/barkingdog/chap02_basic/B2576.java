5f410dad95df342d93741f4b42e52b7e6936a546
dukcode <duk9741@gmail.com>
2022-09-10 08:18:56 +0900

    B2576

---
+++ src/main/java/barkingdog/chap02_basic/B2576.java
@@ -0,0 +1,43 @@
+package barkingdog.chap02_basic;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B2576 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    private static int oddSum;
+    private static int oddMin = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // br = new BufferedReader(new FileReader("input.txt"));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 0; i < 7; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num % 2 == 1) {
+                oddSum += num;
+                oddMin = Math.min(oddMin, num);
+            }
+        }
+
+        if (oddSum == 0) {
+            bw.write(String.valueOf(-1));
+        } else {
+            bw.write(String.valueOf(oddSum));
+            bw.newLine();
+            bw.write(String.valueOf(oddMin));
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
