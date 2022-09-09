9b2dd7044b01c697cc2be95ae7477d86bb0f5021
dukcode <duk9741@gmail.com>
2022-09-09 15:44:04 +0900

    B15486

---
+++ src/main/java/barkingdog/chap15_dp/B15486.java
@@ -0,0 +1,58 @@
+package barkingdog.chap15_dp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B15486 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringTokenizer st;
+
+    private static int n;
+
+    private static int[] t;
+    private static int[] p;
+
+    private static int[] cache;
+
+    private static int ans;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // br = new BufferedReader(new FileReader("input.txt"));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        t = new int[n];
+        p = new int[n];
+
+        cache = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                cache[i] = Math.max(cache[i], cache[i - 1]);
+            }
+
+            if (i + t[i] <= n) {
+                cache[i + t[i]] = Math.max(cache[i + t[i]], cache[i] + p[i]);
+            }
+        }
+
+        bw.write(String.valueOf(Math.max(cache[n - 1], cache[n])));
+
+        bw.close();
+        br.close();
+    }
+
+}
