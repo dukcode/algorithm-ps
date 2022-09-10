f7e002c66a7243d780790aab53134eb53ef3bbc6
dukcode <duk9741@gmail.com>
2022-09-10 09:29:17 +0900

    B2443

---
+++ src/main/java/barkingdog/chap02_basic/B2443.java
@@ -0,0 +1,33 @@
+package barkingdog.chap02_basic;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B2443 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    private static int n;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // br = new BufferedReader(new FileReader("input.txt"));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            bw.write(" ".repeat(i));
+            bw.write("*".repeat(2 * (n - i - 1) + 1));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
