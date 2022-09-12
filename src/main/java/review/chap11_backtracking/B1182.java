eb025166bbe4467ef057316cfe1dcf07278e6a18
dukcode <duk9741@gmail.com>
2022-09-12 12:02:20 +0900

    B1182 review

---
+++ src/main/java/review/chap11_backtracking/B1182.java
@@ -0,0 +1,58 @@
+package review.chap11_backtracking;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B1182 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringTokenizer st;
+
+    private static int n;
+    private static int s;
+    private static int[] arr;
+
+    private static int ans;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // br = new BufferedReader(new FileReader("input.txt"));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        s = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        func(0, 0, 0);
+
+        bw.write(String.valueOf(ans));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void func(int idx, int sum, int count) {
+        if (idx == n) {
+            if (sum == s && count != 0) {
+                ans++;
+            }
+            return;
+        }
+
+        func(idx + 1, sum, count);
+        func(idx + 1, sum + arr[idx], count + 1);
+    }
+
+}
