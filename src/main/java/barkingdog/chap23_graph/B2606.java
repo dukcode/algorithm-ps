5c171eef2f9e1d9aff0296dee1d85af5cc9a8fc0
dukcode <duk9741@gmail.com>
2022-10-06 10:30:55 +0900

    B2606

---
+++ src/main/java/barkingdog/chap23_graph/B2606.java
@@ -0,0 +1,70 @@
+package barkingdog.chap23_graph;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B2606 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringTokenizer st;
+
+    private static int n;
+    private static int m;
+    private static boolean[][] graph;
+    private static boolean[] visited;
+
+    private static Queue<Integer> q = new LinkedList<>();
+
+    private static int ans;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // br = new BufferedReader(new FileReader("input.txt"));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        graph = new boolean[n + 1][n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            graph[s][e] = true;
+            graph[e][s] = true;
+        }
+
+        q.offer(1);
+        visited[1] = true;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (int next = 1; next <= n; ++next) {
+                if (visited[next] || !graph[cur][next]) {
+                    continue;
+                }
+
+                visited[next] = true;
+                q.offer(next);
+                ans++;
+            }
+        }
+
+        bw.write(String.valueOf(ans));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
