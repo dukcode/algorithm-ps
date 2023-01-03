c49e4e6e5823bc22b9a1fb4a3d5f220f590ed67a
dukcode <duk9741@gmail.com>
2023-01-04 05:37:45 +0900

    B2444

---
+++ src/main/java/barkingdog/chap02_basic/B2444.java
@@ -0,0 +1,38 @@
+package barkingdog.chap02_basic;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B2444 {
+	private static BufferedReader br;
+	private static BufferedWriter bw;
+
+	private static int n;
+
+	public static void main(String[] args) throws IOException {
+		// br = new BufferedReader(new FileReader("input.txt"));
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; ++i) {
+			bw.write(" ".repeat(Math.abs((n - 1) - i)));
+			bw.write("*".repeat(Math.abs(2 * i + 1)));
+			bw.newLine();
+		}
+
+		for (int i = n - 2; i >= 0; --i) {
+			bw.write(" ".repeat(Math.abs((n - 1) - i)));
+			bw.write("*".repeat(Math.abs(2 * i + 1)));
+			bw.newLine();
+		}
+
+		br.close();
+		bw.close();
+	}
+
+}
